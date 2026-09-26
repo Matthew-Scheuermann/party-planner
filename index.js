fc26c18a7f6acb73cc97843c16910f85eb0db65a
Matthew Scheuermann <mattscheuer03@gmail.com>
2026-09-25 19:40:01 -0500

    grabbed elements, added event listeners, fetched api, added error message backup

--- index.js
+++ index.js
@@ -16,3 +16,53 @@ appBody.innerHTML = `
     </section>
   </main>
 `;
+
+// grab events
+const upcomingParties = document.querySelector("#events");
+const partyDetails = document.querySelector("#details");
+partyDetails.innerHTML = `<li>Select a party to see details.</li>`;
+
+//click listener
+upcomingParties.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  getPartyDetails(id);
+});
+
+// api fetch
+const getParties = async () => {
+  try {
+    const response = await fetch(
+      "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2608-ftb-ct-web-pt/events",
+    );
+    const result = await response.json();
+    state.parties = result.data;
+
+    upcomingParties.innerHTML = state.parties
+      .map((party) => `<li data-id="${party.id}">${party.name}</li>`)
+      .join("");
+  } catch (error) {
+    upcomingParties.innerHTML = `<li>Something went wrong loading parties.</li>`;
+  }
+};
+
+const getPartyDetails = async (id) => {
+  try {
+    const response = await fetch(
+      `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2608-ftb-ct-web-pt/events/${id}`,
+    );
+    const result = await response.json();
+    state.selectedParty = result.data;
+
+    partyDetails.innerHTML = `
+      <li>${state.selectedParty.name}</li>
+      <li>${state.selectedParty.id}</li>
+      <li>${state.selectedParty.date}</li>
+      <li>${state.selectedParty.description}</li>
+      <li>${state.selectedParty.location}</li>
+    `;
+  } catch (error) {
+    partyDetails.innerHTML = `<li>Something went wrong loading details.</li>`;
+  }
+};
+
+getParties();
